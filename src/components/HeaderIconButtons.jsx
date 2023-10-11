bc67a3b206414406bf9d66518cfb03316478b7e9
riteshmvt <ritesh.mvteams@gmail.com>
2023-10-11 10:01:50 +0530

    func: bookmarking

--- src/components/HeaderIconButtons.jsx
+++ src/components/HeaderIconButtons.jsx
@@ -1,14 +1,17 @@
 import { Link } from "react-router-dom";
 import { AiOutlineHeart, AiOutlineShoppingCart } from "react-icons/ai";
+import { useSelector } from "react-redux";
 
 export default function HeaderIconButtons({ toggleSidebar }) {
+  const bookmark = useSelector((state) => state.bookmark.bookmarkedProducts);
+
   return (
     <div className=" flex items-center gap-6 md:gap-10 ">
       <button className="relative" onClick={toggleSidebar}>
         <AiOutlineHeart size={26} className="text-gray-100" />
 
         <div className="absolute -top-3 -right-2 px-1.5 rounded-md bg-accent-200">
-          <span className="text-sm">0</span>
+          <span className="text-sm">{bookmark.length}</span>
         </div>
       </button>
 
--- src/components/WishlistSidebar.jsx
+++ src/components/WishlistSidebar.jsx
@@ -1,12 +1,21 @@
+import { useNavigate } from "react-router-dom";
 import { MdOutlineClose } from "react-icons/md";
-// import randomImg from "../assets/images/products/2.jpg";
-import Button from "./Button";
 import { useSelector } from "react-redux";
+import Button from "./Button";
 
 export default function WishlistSidebar({ toggleSidebar, sidebarOpen }) {
+  const navigate = useNavigate();
+
   const bookmarkedProducts = useSelector(
     (state) => state.bookmark.bookmarkedProducts
   );
+
+  const navigateProducts = (e) => {
+    e.preventDefault();
+    toggleSidebar();
+    navigate("/products");
+  };
+
   return (
     <>
       <aside
@@ -22,30 +31,42 @@ export default function WishlistSidebar({ toggleSidebar, sidebarOpen }) {
         </button>
         <div className="mt-6 px-2">
           <ul className="flex flex-col gap-4 overflow-y-scroll h-[90vh] hide_scrollbar">
-            {bookmarkedProducts.map((product, i) => {
-              return (
-                <li
-                  key={i}
-                  className="flex px-2 py-4 bg-deep-100 rounded-md gap-3"
-                >
-                  <figure className="w-36 h-36 rounded-md overflow-hidden">
-                    <img src={product.img} alt="img" />
-                  </figure>
-                  <div className="grow flex flex-col gap-2">
-                    <div className="flex justify-between">
-                      <h4 className="text-xl capitalize font-medium text-gray-100">
-                        {product.description}
-                      </h4>
-                      <MdOutlineClose size={26} />
+            {bookmarkedProducts.length > 0 ? (
+              bookmarkedProducts.map((product, i) => {
+                return (
+                  <li
+                    key={i}
+                    className="flex px-2 py-4 bg-deep-100 rounded-md gap-3"
+                  >
+                    <figure className="w-36 h-36 rounded-md overflow-hidden">
+                      <img src={product.img} alt="img" />
+                    </figure>
+                    <div className="grow flex flex-col gap-2">
+                      <div className="flex justify-between">
+                        <h4 className="text-xl capitalize font-medium text-gray-100">
+                          {product.description}
+                        </h4>
+                        <MdOutlineClose size={26} />
+                      </div>
+                      <p className="text-accent-200">${product.price}</p>
+                      <Button className="text-deep-300 bg-accent-200 hover:bg-accent-300 mt-auto text-xs">
+                        Add to cart
+                      </Button>
                     </div>
-                    <p className="text-accent-200">${product.price}</p>
-                    <Button className="text-deep-300 bg-accent-200 hover:bg-accent-300 mt-auto text-xs">
-                      Add to cart
-                    </Button>
-                  </div>
-                </li>
-              );
-            })}
+                  </li>
+                );
+              })
+            ) : (
+              <div className="flex flex-col gap-4 h-[50vh] justify-center items-center">
+                <h3 className="text-3xl font-semibold">No products Saved</h3>
+                <Button
+                  onClick={(e) => navigateProducts(e)}
+                  className="text-deep-300 bg-accent-200 hover:bg-accent-300"
+                >
+                  Shop now
+                </Button>
+              </div>
+            )}
           </ul>
         </div>
       </aside>
